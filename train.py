40e363ce07cb9ab06c984399e03cffc6f7c607cc
Christian Schultze <christian.schultze98@gmx.de>
2022-11-06 15:40:16 +0100

    [WIP] do refactoring

--- train.py
+++ train.py
@@ -29,6 +29,8 @@ def train(load_model=None, save_model=None):
     :return: None
     """
 
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+
     # create model
     model = DhSegment([3, 3, 4, 3], in_channels=IN_CHANNELS, out_channel=OUT_CHANNELS, load_resnet_weights=True)
     # [3, 4, 6, 4]
@@ -37,7 +39,6 @@ def train(load_model=None, save_model=None):
     # load model if argument is None it does nothing
     model.load(load_model)
 
-    device = "cuda" if torch.cuda.is_available() else "cpu"
     print(f"Using {device} device")
 
     if device == 'cuda':
@@ -56,10 +57,10 @@ def train(load_model=None, save_model=None):
     optimizer = AdamW(model.parameters(), lr=LEARNING_RATE)
     loss_fn = CrossEntropyLoss()  # weight=torch.tensor(LOSS_WEIGHTS)
 
-    # train
-    for e in range(1, EPOCHS + 1):
-        print(f"start epoch {e}:")
-        train_loop(train_set, model, loss_fn, optimizer)
+    train_loader = torch.utils.data.DataLoader(train_set, batch_size=BATCH_SIZE, shuffle=True,
+                                               num_workers=DATALOADER_WORKER)
+
+    train_loop(train_loader, len(train_set), model, loss_fn, optimizer)
 
     model.save(save_model)
 
@@ -67,48 +68,48 @@ def train(load_model=None, save_model=None):
     validation(test_set, model, loss_fn)
 
 
-def train_loop(data: NewsDataset, model: torch.nn.Module, loss_fn: torch.nn.Module, optimizer: torch.optim.Optimizer):
+def train_loop(train_loader: DataLoader, n_train: int, model: torch.nn.Module, loss_fn: torch.nn.Module,
+               optimizer: torch.optim.Optimizer):
     """
-    executes one trainings epoch
-    :param data: Dataloader object with data to train on
+    executes one training epoch
+    :param train_loader: Dataloader object
+    :param n_train: size of train data
     :param model: model to train
     :param loss_fn: loss function to optimize
     :param optimizer: optimizer to use
     :return: None
     """
-    device = "cuda" if torch.cuda.is_available() else "cpu"
 
-    size = len(data)
-    data = torch.utils.data.DataLoader(data, batch_size=BATCH_SIZE, shuffle=True, num_workers=DATALOADER_WORKER)
+    for epoch in range(1, EPOCHS + 1):
+        model.train()
+        # rolling_average = RollingAverage(10)
 
-    rolling_average = RollingAverage(10)
-    t = tqdm(data, desc='train_loop', total=size)
-    for images, masks, _ in t:
-        # print(f"{X.shape=}")
-        # print(f"start: {torch.cuda.memory_reserved()=}")
-        images = images.to(device)
-        masks = masks.to(device)
+        with tqdm(total=n_train, desc=f'Epoch {epoch}/{EPOCHS}', unit='img') as pbar:
+            print(torch.cuda.memory_summary(device=DEVICE, abbreviated=False))
+            for batch in train_loader:
+                images = batch[0]
+                true_masks = batch[1]
 
-        # Compute prediction and loss
-        pred = model(images)
-        # print(f"pred: {torch.cuda.memory_reserved()=}")
-        loss = loss_fn(pred, masks)
-        # print(f"loss: {torch.cuda.memory_reserved()=}")
-        # Backpropagation
-        optimizer.zero_grad()
-        loss.backward()
-        # print(f"backward: {torch.cuda.memory_reserved()=}")
-        optimizer.step()
-        # print(f"optimizer: {torch.cuda.memory_reserved()=}")
-        # update description
-        t.set_description(f"loss: {rolling_average(loss.detach())}")
-        del images, masks, pred, loss
-        torch.cuda.empty_cache()
-        # print(f"end: {torch.cuda.memory_reserved()=}")
-        # print()
+                # print(f"{X.shape=}")
+                # print(f"start: {torch.cuda.memory_reserved()=}")
+                images = images.to(DEVICE)
+                true_masks = true_masks.to(DEVICE)
 
-        del images, masks, pred, loss
-        torch.cuda.empty_cache()
+                # Compute prediction and loss
+                pred = model(images)
+                loss = loss_fn(pred, true_masks)
+                # Backpropagation
+                optimizer.zero_grad(set_to_none=True)
+                loss.backward()
+                optimizer.step()
+                # print(f"optimizer: {torch.cuda.memory_reserved()=}")
+                # update description
+                pbar.update(images.shape[0])
+                pbar.set_postfix(**{'loss (batch)': loss.item()})
+                del images, true_masks, pred, loss
+                torch.cuda.empty_cache()
+                # print(f"end: {torch.cuda.memory_reserved()=}")
+                # print()
 
 
 def validation(data: NewsDataset, model, loss_fn):
@@ -154,4 +155,5 @@ def validation(data: NewsDataset, model, loss_fn):
 
 
 if __name__ == '__main__':
+    DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
     train(load_model=None, save_model='Models/model.pt')
