71695c1baca18154e9d08bab3eb0c47166348c3c
Niklas Kerkfeld <s6nikerk@uni-bonn.de>
2022-12-09 19:23:42 +0100

    added script to create crops and save them

---
+++ create_crops.py
@@ -0,0 +1,47 @@
+import os
+
+import numpy as np
+import torch
+from PIL import Image  # type: ignore
+from tqdm import tqdm
+
+from preprocessing import Preprocessing
+
+INPUT = "../prima/inputs/"
+TARGETS = "../prima/targets/"
+
+FOLDER = "crops/"
+
+
+def main():
+    preprocessing = Preprocessing()
+
+    # read all file names
+    paths = [f[:-4] for f in os.listdir(INPUT) if f.endswith(".tif")]
+    print(f"{len(paths)=}")
+
+    # iterate over files
+    for file in tqdm(paths, desc='cropping images', unit='image'):
+        # load image
+        image = Image.open(f"{INPUT}{file}.tif").convert('RGB')
+
+        # load target
+        target = np.load(f"{TARGETS}pc-{file}.npy")
+
+        assert image.size[1] == target.shape[0] and image.size[0] == target.shape[1], \
+            f"image {file=} has shape {image.size}, but target has shape {target.shape}"
+
+        # preprocess / create crops
+        img_crops, tar_crops = preprocessing.preprocess(image, target)
+
+        for i, (img_crop, tar_crop) in enumerate(zip(img_crops, tar_crops)):
+            img_crop = torch.tensor(img_crop, dtype=torch.uint8)
+            tar_crop = torch.tensor(tar_crop, dtype=torch.uint8)
+
+            torch.save(img_crop, f"{FOLDER}Images/{file}_{i}.pt")
+            torch.save(tar_crop, f"{FOLDER}Targets/{file}_{i}.pt")
+
+
+if __name__ == '__main__':
+    main()
+
