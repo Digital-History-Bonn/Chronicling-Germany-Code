8cbd27e9dd6b95b7e9bc9869240fbeea62e3bccb
Christian Schultze <christian.schultze.98@gmx.de>
2023-08-28 14:20:37 +0200

    fix tensor concat

--- tests/test_newsdataset.py
+++ tests/test_newsdataset.py
@@ -48,8 +48,8 @@ class TestClassNewsdataset:
                 break
             news_data.append(data[0])
             news_targets.append(data[1])
-        news_data = torch.cat(news_data)
-        news_targets = torch.cat(news_targets)
+        news_data = torch.stack(news_data)
+        news_targets = torch.stack(news_targets)
         ground_truth_data = torch.load(f"{DATA_PATH}output/news_data.pt")
         ground_truth_tragets = torch.load(f"{DATA_PATH}output/news_targets.pt")
 
