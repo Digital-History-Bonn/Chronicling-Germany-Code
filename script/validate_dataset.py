849954df5cb6898503550f77df9abf27285534c9
Christian Schultze <christian.schultze.98@gmx.de>
2023-11-02 11:26:40 +0100

    validate dataset can now count classes

--- script/validate_dataset.py
+++ script/validate_dataset.py
@@ -2,8 +2,11 @@
 import argparse
 import os
 
+import torch
 from tqdm import tqdm
+from torch.utils.data import DataLoader
 
+from src.news_seg.news_dataset import NewsDataset
 from src.news_seg.preprocessing import (
     Preprocessing,
 )
@@ -60,6 +63,22 @@ def validate(args):
             print(f"{file}")
 
 
+def count_classes(args: argparse.Namespace):
+    """Load data with increasing amount of workers"""
+    preprocessing = Preprocessing(scale=0.5, crop_factor=1, crop_size=512)
+    dataset = NewsDataset(preprocessing, image_path=f"{args.data_path}images/",
+                          target_path=f"{args.data_path}targets/",
+                          dataset=args.dataset)
+    dataset.augmentations = False
+
+    loader = DataLoader(dataset, shuffle=False, num_workers=args.num_workers, batch_size=args.batch_size)
+    class_counts = torch.empty((10), dtype=torch.long)
+    for _, targets in tqdm(loader, desc="counting classes", unit="batches"):
+        class_counts += torch.bincount(targets.flatten(), minlength=10)
+    print(class_counts)
+    print(class_counts / torch.sum(class_counts))
+
+
 def get_args() -> argparse.Namespace:
     """defines arguments"""
     parser = argparse.ArgumentParser(description="train")
@@ -79,10 +98,35 @@ def get_args() -> argparse.Namespace:
         help="which dataset to expect. Options are 'transcribus' and 'HLNA2013' "
              "(europeaner newspaper project)",
     )
+    parser.add_argument(
+        "--classes",
+        "-c",
+        action="store_true",
+        help="If activated, pixels of each class are counted.",
+    )
+    parser.add_argument(
+        "--num-workers",
+        "-w",
+        type=int,
+        default=1,
+        help="Number of workers for the Dataloader",
+    )
+    parser.add_argument(
+        "--batch-size",
+        "-b",
+        dest="batch_size",
+        metavar="B",
+        type=int,
+        default=1,
+        help="Batch size",
+    )
 
     return parser.parse_args()
 
 
 if __name__ == "__main__":
     parameter_args = get_args()
-    validate(parameter_args)
+    if parameter_args.classes:
+        count_classes(parameter_args)
+    else:
+        validate(parameter_args)
