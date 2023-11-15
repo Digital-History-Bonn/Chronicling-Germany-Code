2cff01a5b56aeda231fbfe36dfb4b6b01e69c2dc
Christian Schultze <christian.schultze.98@gmx.de>
2023-11-15 11:43:29 +0100

    slices calculation now only pays attention to necessary classes

--- script/transkribus_export.py
+++ script/transkribus_export.py
@@ -26,12 +26,19 @@ def create_sub_masks(pred: ndarray) -> Dict[int, Image.Image]:
     return sub_masks
 
 
-def create_polygons(sub_mask: ndarray, label: int, tolerance: List[float], bbox_size: int) -> Tuple[
+def create_polygons(sub_mask: ndarray, label: int, tolerance: List[float], bbox_size: int, export: bool) -> Tuple[
     List[List[float]], List[List[float]]]:
     """Find contours (boundary lines) around each sub-mask
     # Note: there could be multiple contours if the object
     # is partially occluded. (E.g., an elephant behind a tree)
     # from https://www.immersivelimit.com/tutorials/create-coco-annotations-from-scratch/#create-custom-coco-dataset
+    :param sub_mask: current submask, from that polygons will be calculated
+    :param label: current class label
+    :param tolerance: tolerance array which contains pixel toplerance for simplifying polygons
+    :param bbox_size: size to which the edges must at least sum to
+    :param export: wheter transkribus export or output_path is activated.
+    If this is not the case, polygon simplification ist not necessary.
+    :return:
     """
     contours = measure.find_contours(sub_mask, 0.5, positive_orientation="low")
     segmentations: List[List[float]] = []
@@ -45,7 +52,8 @@ def create_polygons(sub_mask: ndarray, label: int, tolerance: List[float], bbox_
 
         # Make a polygon and simplify it
         poly = Polygon(contour)
-        poly = poly.simplify(tolerance[label - 1], preserve_topology=False)
+        if export:
+            poly = poly.simplify(tolerance[label - 1], preserve_topology=False)
         if poly.geom_type == 'MultiPolygon':
             multi_polygons = list(poly.geoms)
             for polygon in multi_polygons:
@@ -63,6 +71,7 @@ def append_polygons(poly: Polygon, bbox_list: List[List[float]], segmentations: 
     :param bbox_list: List containing bbox List with uppper left and lower right corner.
     :param poly: polygon
     :param segmentations: List containing polygons
+    :param bbox_size: size to which the edges must at least sum to
     """
     segmentation = np.array(poly.exterior.coords).ravel().tolist()
     if len(segmentation) > 2:
@@ -86,12 +95,15 @@ def bbox_sufficient(bbox: List[float], size: int, x_axis: bool = False) -> bool:
     return (bbox[2] - bbox[0]) + (bbox[3] - bbox[1]) > size
 
 
-def prediction_to_polygons(pred: ndarray, tolerance: List[float], bbox_size: int) -> Tuple[
+def prediction_to_polygons(pred: ndarray, tolerance: List[float], bbox_size: int, export: bool) -> Tuple[
     Dict[int, List[List[float]]], Dict[int, List[List[float]]]]:
     """
     Converts prediction int ndarray to a dictionary of polygons
     :param tolerance: Array with pixel tolarance values for poygon simplification
     :param pred: prediction ndarray
+    :param export: wheter transkribus export or output_path is activated.
+        If this is not athe case, only the article and horizontal seperator class bboxes are of relevance.
+        Everything else will be skipped to improve performance.
     """
     masks = create_sub_masks(pred)
 
@@ -100,8 +112,9 @@ def prediction_to_polygons(pred: ndarray, tolerance: List[float], bbox_size: int
     for label, mask in masks.items():
         # debug masks
         # mask.save(f"data/output/{label}.png")
-        segment, bbox = create_polygons(np.array(mask), label, tolerance, bbox_size)
-        segmentations[label], bbox_dict[label] = segment, bbox
+        if export or label == 4 or label == 9:
+            segment, bbox = create_polygons(np.array(mask), label, tolerance, bbox_size, export)
+            segmentations[label], bbox_dict[label] = segment, bbox
     return segmentations, bbox_dict
 
 
