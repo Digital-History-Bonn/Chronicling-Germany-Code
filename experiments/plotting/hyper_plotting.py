9f622d07a5d8a2dce651c43a6ffb5658fbeaccce
Christian Schultze <christian.schultze.98@gmx.de>
2023-11-22 18:26:46 +0100

    update plotting

--- experiments/plotting/hyper_plotting.py
+++ experiments/plotting/hyper_plotting.py
@@ -37,35 +37,43 @@ def plot_2d(data: ndarray, error, name: str) -> None:
     main_color = ['tab:blue', 'tab:orange', 'tab:green', 'tab:red', 'tab:purple']
     background_color = ['lightsteelblue', 'peachpuff', 'palegreen', 'tab:red', 'tab:purple']
 
-    plt.plot(data[:, 1], data[:, 2], color=main_color[0], label=labels[0])
-    plt.plot(data[:, 1], data[:, 3], color=main_color[1], label=labels[1])
-    plt.fill_between(data[:, 1], data[:, 2] - error[:, 2], data[:, 2] + error[:, 2], color=background_color[0])
-    plt.fill_between(data[:, 1], data[:, 3] - error[:, 3], data[:, 3] + error[:, 3], color=background_color[1])
-    plt.title("Learning Rate")
+    plt.plot(data[:, 0], data[:, 2], color=main_color[0], label=labels[0])
+    plt.plot(data[:, 0], data[:, 3], color=main_color[1], label=labels[1])
+    plt.fill_between(data[:, 0], data[:, 2] - error[:, 2], data[:, 2] + error[:, 2], color=background_color[0])
+    plt.fill_between(data[:, 0], data[:, 3] - error[:, 3], data[:, 3] + error[:, 3], color=background_color[1])
+    plt.title("Batch Size")
 
-    plt.xscale("log")
+    single_data = np.array([[0.9563, 0.9573, 0.9579], [0.5807, 0.5671, 0.5863]])
+    single_mean = np.mean(single_data, axis=1)
+    single_std = np.std(single_data, axis=1)
+    plt.errorbar([16], single_mean[0], yerr=single_std[0], fmt='o', color=main_color[0])
+    plt.errorbar([16], single_mean[1], yerr=single_std[1], fmt='o', color=main_color[1])
 
-    plt.xlabel("Learning Rate")
-    plt.legend(loc="upper right")
+    plt.xticks([16, 32, 64], [16, 32, 64])
+
+    plt.xlabel("Batch Size")
+    plt.legend(loc="right")
+
+    plt.grid()
 
     plt.savefig(f"{name}.pdf")
     fig = plt.gcf()
     fig = tikzplotlib_fix_ncols(fig)
-    tikzplotlib.clean_figure()
+    # tikzplotlib.clean_figure()
     tikzplotlib.save(f"{name}.tex")
 
 
 def main():
-    values_1 = np.roll(np.array(load_json("logs/learning-rate-test/")), 1, axis=0)
-    values_2 = np.roll(np.array(load_json("logs/learning-rate-B/")), 1, axis=0)
-    values_3 = np.roll(np.array(load_json("logs/learning-rate-C/")), 1, axis=0)
+    values_1 = np.roll(np.array(load_json("logs/batch_A/")), 0, axis=0)
+    values_2 = np.roll(np.array(load_json("logs/batch_B/")), 0, axis=0)
+    values_3 = np.roll(np.array(load_json("logs/batch_C/")), 0, axis=0)
 
     data = np.stack([values_1, values_2, values_3])
     mean_data = np.mean(data, axis=0)
     error = np.std(data, axis=0)
 
     # print(np.argmax(data_ndarray))
-    plot_2d(mean_data, error, "lr-hyper")
+    plot_2d(mean_data, error, "batch-hyper")
 
 
 if __name__ == "__main__":
