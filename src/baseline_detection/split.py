2c0ff2e9c970b739a99d885748e3734d3b924bd5
NiklasKerkfeld <s6nikerk@uni-bonn.de>
2024-05-28 16:40:57 +0200

    added args to split

--- src/baseline_detection/split.py
+++ src/baseline_detection/split.py
@@ -1,69 +1,91 @@
 """Splits data into training, validation and test split."""
-
-import glob
+import argparse
+import json
 import os
-import random
 import shutil
-from pathlib import Path
-from typing import List, Tuple
+from typing import List
 
 from tqdm import tqdm
 
 
-def copy(folder: str, split: List[str], name: str) -> None:
+def get_args() -> argparse.Namespace:
+    """
+    Defines arguments.
+
+    Returns:
+        Namespace with parsed arguments.
+    """
+    parser = argparse.ArgumentParser(description="preprocess")
+    parser.add_argument(
+        "--image_dir",
+        "-i",
+        type=str,
+        default=None,
+        help="path for folder with images. Images need to be jpg."
+    )
+
+    parser.add_argument(
+        "--annotation_dir",
+        "-a",
+        type=str,
+        default=None,
+        help="path for folder with layout xml files."
+    )
+
+    parser.add_argument(
+        "--output_dir",
+        "-o",
+        type=str,
+        default=None,
+        help="path to the folder where to save the preprocessed trainings targets",
+    )
+
+    return parser.parse_args()
+
+
+def copy(folder: str, split: List[str]) -> None:
     """
     Copies the files form a given split to a new folder.
 
     Args:
         folder: folder to copy the files form
         split: list of files to copy
-        name: name of the new folder (should be 'train', 'valid', 'test')
     """
-    os.makedirs(f"{folder}/../{name}")
+    os.makedirs(folder, exist_ok=True)
 
     for f in tqdm(split, desc="copying"):
-        numb = f.split(os.sep)[-1]
-        shutil.copytree(f, f"{folder}/../{name}/{numb}")
+        shutil.copy(f, f"{folder}/{os.path.basename(f)}")
 
 
-def split_dataset(folder: str, split: Tuple[float, float, float]) -> None:
-    """
-    Splits the dataset into training, validation and test split.
+def main() -> None:
+    """Splits the dataset into training, validation and test split."""
+    args = get_args()
+    output_dir = args.output_dir
+    image_dir = args.image_dir
+    annotation_dir = args.annotation_dir
 
-    Args:
-        folder: folder with train dataset
-        split: Tuple with partitions of splits (should sum up to 1)
-    """
-    # seed the process for reproducibility
-    random.seed(42)
+    with open("data/neurips-split.json", 'r',
+              encoding='utf-8') as file:
+        data = json.load(file)
 
-    # get folder with datapoints form train dataset
-    files = list(glob.glob(f"{folder}/*"))
-    print(f"{folder}/*")
-    print(f"{len(files)=}")
+    train_files = data.get('Training', [])
+    valid_files = data.get('Validation', [])
+    test_files = data.get('Test', [])
 
-    # shuffle
-    shuffled_list = random.sample(files, len(files))
-
-    # calc split indices
-    n = len(files)
-    s1, s2 = int(n * split[0]), int(n * (split[0] + split[1]))
-
-    # create splits
-    train = shuffled_list[:s1]
-    valid = shuffled_list[s1:s2]
-    test = shuffled_list[s2:]
-
-    print(f"{len(train)=}")
-    print(f"{len(valid)=}")
-    print(f"{len(test)=}")
+    print(f"{len(train_files)=}")
+    print(f"{len(valid_files)=}")
+    print(f"{len(test_files)=}")
 
     # copy data in 3 new folder
-    copy(folder, train, "train_mask")
-    copy(folder, valid, "valid_mask")
-    copy(folder, test, "test_mask")
+    copy(f"{output_dir}/train", [f"{image_dir}/{x}.jpg" for x in train_files])
+    copy(f"{output_dir}/train", [f"{annotation_dir}/{x}.npz" for x in train_files])
+
+    copy(f"{output_dir}/valid", [f"{image_dir}/{x}.jpg" for x in valid_files])
+    copy(f"{output_dir}/valid", [f"{annotation_dir}/{x}.npz" for x in valid_files])
+
+    copy(f"{output_dir}/test", [f"{image_dir}/{x}.jpg" for x in test_files])
+    copy(f"{output_dir}/test", [f"{annotation_dir}/{x}.npz" for x in test_files])
 
 
 if __name__ == "__main__":
-    split_dataset(f"{Path(__file__).parent.absolute()}/../../data/preprocessed",
-                  (0.8, 0.1, 0.1))
+    main()
