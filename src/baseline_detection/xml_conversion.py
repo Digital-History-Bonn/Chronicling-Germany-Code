0227a422b7960fbcbcc65232b82076f125f384b0
NiklasKerkfeld <s6nikerk@uni-bonn.de>
2024-10-11 19:14:55 +0200

    baseline prediction improvement

--- src/baseline_detection/xml_conversion.py
+++ src/baseline_detection/xml_conversion.py
@@ -1,29 +1,13 @@
 """Script to convert baseline predictions into xml format."""
 
-from typing import List, Tuple
+from typing import List
 
 import numpy as np
-from bs4 import BeautifulSoup, PageElement
+from bs4 import BeautifulSoup
 from shapely import LineString
 from shapely.geometry import Polygon
 
-from src.baseline_detection.utils import convert_coord, order_lines
-
-
-def get_textregions(page: PageElement) -> Tuple[List[PageElement], List[Polygon]]:
-    """
-    Gets all text regions and their corresponding polygon.
-
-    Args:
-        page (PageElement): Page element with text regions.
-
-    Returns:
-        text_regions (List[PageElement]): List of text regions.
-        polygons (List[Polygon]): List of polygons.
-    """
-    text_regions = page.find_all('TextRegion')
-    polygons = [Polygon(convert_coord(elem)) for elem in text_regions]
-    return text_regions, polygons
+from src.baseline_detection.utils import order_lines
 
 
 def polygon_to_string(input_list: List[float]) -> str:
@@ -51,15 +35,15 @@ def polygon_to_string(input_list: List[float]) -> str:
 
 def add_baselines(layout_xml: str,
                   output_file: str,
-                  textlines: List[Polygon], baselines: List[LineString]) -> None:
+                  textlines: List[List[Polygon]], baselines: List[List[LineString]]) -> None:
     """
     Adds testline and baseline prediction form model to the layout xml file.
 
     Args:
         layout_xml: path to layout xml file
         output_file: path to output xml file
-        textlines: list of shapely LineString predicted by model
-        baselines: list of shapely baselines predicted by model
+        textlines: list of list of shapely LineString predicted by model
+        baselines: list of list of shapely baselines predicted by model
     """
     with open(layout_xml, "r", encoding="utf-8") as file:
         data = file.read()
@@ -73,32 +57,28 @@ def add_baselines(layout_xml: str,
     for page_element in page_elements:
         page_element.decompose()
 
-    textregions, polygones = get_textregions(page)
+    textregions = page.find_all('TextRegion')
 
     # adds all predicted textlines to annotation if they have their center inside a text region
-    counter_dict = {i: 0 for i in range(len(textregions))}
-    for textline, baseline in zip(textlines, baselines):
-        for j, (textregion, polygon) in enumerate(zip(textregions, polygones)):
-            if polygon.contains(textline.centroid):
-                counter_dict[j] += 1
-                new_textline = soup.new_tag('TextLine')
-                new_textline['custom'] = f"readingOrder {{index:{counter_dict[j]};}}"
-                new_textline['id'] = textregion['id'] + f'_tl_{counter_dict[j]}'
+    for textregion, region_textlines, region_baselines in zip(textregions, textlines, baselines):
+        for i, (textline, baseline) in enumerate(zip(region_textlines, region_baselines)):
+            new_textline = soup.new_tag('TextLine')
+            new_textline['custom'] = f"readingOrder {{index:{i};}}"
+            new_textline['id'] = textregion['id'] + f'_tl_{i}'
 
-                # add textline
-                coords_element = soup.new_tag("Coords")
-                points_list = np.array(textline.exterior.coords)[:, ::-1].ravel().tolist()
-                coords_element["points"] = polygon_to_string(points_list)
-                new_textline.append(coords_element)
+            # add textline
+            coords_element = soup.new_tag("Coords")
+            points_list = np.array(textline.exterior.coords)[:, ::-1].ravel().tolist()
+            coords_element["points"] = polygon_to_string(points_list)
+            new_textline.append(coords_element)
 
-                # add baseline
-                baseline_element = soup.new_tag("Baseline")
-                points_list = np.array(baseline.coords)[:, ::-1].ravel().tolist()
-                baseline_element["points"] = polygon_to_string(points_list)
-                new_textline.append(baseline_element)
+            # add baseline
+            baseline_element = soup.new_tag("Baseline")
+            points_list = np.array(baseline.coords)[:, ::-1].ravel().tolist()
+            baseline_element["points"] = polygon_to_string(points_list)
+            new_textline.append(baseline_element)
 
-                textregion.append(new_textline)
-                break
+            textregion.append(new_textline)
 
     for region in textregions:
         order_lines(region)
