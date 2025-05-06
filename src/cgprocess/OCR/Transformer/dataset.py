22094a10feeca49f21bf24423be99d49d30fccf4
NKerkfeld <s6nikerk@uni-bonn.de>
2025-05-06 14:11:26 +0200

    add augmentation

--- src/cgprocess/OCR/Transformer/dataset.py
+++ src/cgprocess/OCR/Transformer/dataset.py
@@ -8,19 +8,58 @@ import torch
 import torch.nn.functional as F
 from bs4 import BeautifulSoup
 from tqdm import tqdm
+import cv2
+from albumentations import Blur, Compose, ElasticTransform, MedianBlur, MotionBlur, OneOf, \
+    SafeRotate, OpticalDistortion, PixelDropout, ToFloat
 
-from src.cgprocess.OCR.Transformer.config import PAD_HEIGHT, PAD_WIDTH, MAX_SEQUENCE_LENGTH, ALPHABET
+from src.cgprocess.OCR.Transformer.config import PAD_HEIGHT, PAD_WIDTH, MAX_SEQUENCE_LENGTH, \
+    ALPHABET
 from src.cgprocess.OCR.Transformer.tokenizer import Tokenizer
 from src.cgprocess.OCR.utils import get_bbox, load_image
 from src.cgprocess.layout_segmentation.processing.read_xml import xml_polygon_to_polygon_list
+
+
+class DefaultAugmenter:
+    """Default Augmenter form kraken.
+
+    Source: https://github.com/mittagessen/kraken/blob/main/kraken/lib/dataset/recognition.py
+
+    """
+
+    def __init__(self):
+        cv2.setNumThreads(0)
+
+        self._transforms = Compose([
+            ToFloat(),
+            PixelDropout(p=0.2),
+            OneOf([
+                MotionBlur(p=0.2),
+                MedianBlur(blur_limit=3, p=0.1),
+                Blur(blur_limit=3, p=0.1),
+            ], p=0.2),
+            OneOf([
+                OpticalDistortion(p=0.3),
+                ElasticTransform(alpha=7, sigma=25, p=0.1),
+                SafeRotate(limit=(-3, 3), border_mode=cv2.BORDER_CONSTANT, p=0.2)
+            ], p=0.2),
+        ], p=0.5)
+
+    def __call__(self, image):
+        im = image.permute((1, 2, 0)).numpy()
+        o = self._transforms(image=im)
+
+        return torch.tensor(o['image'].transpose(2, 0, 1))
 
 
 class Dataset(torch.utils.data.Dataset):
     """
     Dataset class for Transformer based OCR.
     """
-    def __init__(self, image_path: str,
+
+    def __init__(self,
+                 image_path: str,
                  target_path: str,
+                 augmentations: bool = True,
                  pad_seq: bool = False,
                  cache_images: bool = False):
         """
@@ -33,6 +72,7 @@ class Dataset(torch.utils.data.Dataset):
         """
         self.image_path = image_path
         self.target_path = target_path
+        self.augmentations = DefaultAugmenter() if augmentations else None
         self.cache_images = cache_images
 
         self.tokenizer = Tokenizer(ALPHABET, pad=pad_seq, max_length=MAX_SEQUENCE_LENGTH)
@@ -84,6 +124,9 @@ class Dataset(torch.utils.data.Dataset):
         crop = F.pad(crop, (pad_width, 0, pad_height, 0), "constant", 0)
         crop = crop[:, :PAD_HEIGHT]
 
+        if self.augmentations:
+            crop = self.augmentations(crop)
+
         return crop.float() / 255, target, text
 
 
@@ -116,6 +159,13 @@ def read_xml(xml_path: str) -> Tuple[List[torch.Tensor], List[str]]:
 
 
 if __name__ == '__main__':
+    import matplotlib.pyplot as plt
+
     dataset = Dataset(image_path='data/preprocessedOCR/train',
                       target_path='data/preprocessedOCR/train',
                       cache_images=True)
+
+    crop, target, text = dataset[0]
+    plt.imshow(crop)
+    plt.savefig('data/deleteMe/crop.png')
+    plt.close()
